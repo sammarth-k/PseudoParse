6f892cb3be4ea7f5d4792fd8ef045428638c4ded
Sammarth Kumar <47685020+sammarth-k@users.noreply.github.com>
2021-05-22 21:13:54 +0530

    Add files via upload

---
+++ tokens.py
@@ -0,0 +1,41 @@
+#Tokens
+TOKEN_STEP = "step"
+
+#Variables
+TOKEN_VAR = "set"
+
+#Comparison
+TOKEN_E = "=="
+TOKEN_NE = "!="
+TOKEN_LT = "<"
+TOKEN_GT =">"
+TOKEN_GTE = ">="
+TOKEN_LTE = "<="
+
+#Operators
+TOKEN_PLUS = "+"
+TOKEN_MINUS = "-"
+TOKEN_MULT = "*"
+TOKEN_DIV = "/"
+TOKEN_MOD = "%"
+
+#Conditionals
+TOKEN_IF = "if"
+TOKEN_ELSE = "else"
+
+#Membership
+TOKEN_IN = "in"
+TOKEN_NOT_IN = "not-in"
+
+#Loops
+TOKEN_LOOP = "while"
+TOKEN_REPEAT = "repeat"
+TOKEN_BREAK = "break"
+TOKEN_CONTINUE =  "continue"
+
+#Basic commands
+TOKEN_PRINT="print"
+TOKEN_INPUT="input"
+
+#Comment
+TOKEN_COMMENT="#"
